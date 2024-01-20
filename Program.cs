d6e0ada9edd462a6ad4194e782072deeeb10a3eb
Ahmad Baker <ahmadqadoomy79@gmail.com>
2024-01-20 13:54:39 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace lab0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating Variables
+            Console.WriteLine("Enter a low number:");
+            int lowNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a higher number");
+            int highNumber = Convert.ToInt32(Console.ReadLine());
+
+            int difference = highNumber - lowNumber;
+            Console.WriteLine($"Your low number is {lowNumber} and your high number is {highNumber} and the difference between them is {difference}");
+
+            
+        }
+    }
+
+}
